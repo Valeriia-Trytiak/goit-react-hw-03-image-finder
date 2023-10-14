a0c5d2f5a7ab2d73c47b4d7ceb6b3a81e40146c1
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-10-14 17:15:53 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { ColorRing } from 'react-loader-spinner';
+import toast, { Toaster } from 'react-hot-toast';
 
 import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
@@ -20,33 +21,33 @@ uppdateSearchbar = (searchName)=> {
   }));
 }
 
-async componentDidUpdate(prevProps, prevState) { 
-try {
-  if (
-    prevState.searchValue !== this.state.searchValue &&
-    this.state.searchValue !== null &&
-    this.state.searchValue !== undefined
-  ) 
-  {
-  this.setState({isLoading: true});
-  const searchImg = await fechServisSearchImg(this.state.searchValue);
-  this.setState({
-  gallery: searchImg.hits,
-  });
+async componentDidUpdate(prevProps, prevState) {
+  try {
+    if (
+      prevState.searchValue !== this.state.searchValue &&
+      this.state.searchValue !== null &&
+      this.state.searchValue !== undefined
+    ) {
+      this.setState({ isLoading: true, error: false });
+      const searchImg = await fechServisSearchImg(this.state.searchValue);
+      toast.success("Images found successfully!")
+      this.setState({
+        gallery: searchImg.hits,
+        isLoading: false, 
+        error: false,
+      });
+    }
+  } catch (error) {
+    this.setState({ error: true, isLoading: false }); 
   }
-} catch (error) {
-  this.setState({ error: true });
-} finally{
-  this.setState({ isLoading: false});
-}
-
 }
 
   render () {
-    const { gallery, isLoading }= this.state;
+    const { gallery, isLoading, error }= this.state;
 
-    return <div>
-    <Searchbar onSubmit={this.uppdateSearchbar}></Searchbar>
+  return <div>
+  <Searchbar onSubmit={this.uppdateSearchbar}></Searchbar>
+
   {isLoading && <ColorRing
   visible={true}
   height="80"
@@ -55,11 +56,12 @@ try {
   wrapperStyle={{}}
   wrapperClass="blocks-wrapper"
   colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-/>}
-    {gallery.length > 0 && <ImageGallery galleryImages = {gallery} /> }
-      
-    
-    
+  />}
+
+  {error && <span>Whoops... Error! Please, reload this page!</span>}
+
+  {gallery.length > 0 && <ImageGallery galleryImages = {gallery} /> }
+  <Toaster  position="top-right" /> 
   </div>
   } 
 };
